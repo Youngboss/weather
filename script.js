54466ce72ecf12df4f8545d2f7e277d02fdba1bd
Dave Allen <allendave007@gmail.com>
2023-04-03 22:51:48 -0400

    working on it

--- script.js
+++ script.js
@@ -19,15 +19,17 @@ var submitSearch = function(){
         if (cityLat !=0 && cityLon !=0) {
             var Url = "https://api.openweathermap.org/data/2.5/weather?lat="+cityLat+"&lon="+cityLon+"&appid="+apiKey+"&units=imperial"
                 $.get(Url,null,function(weatherData){
-                console.log(weatherData)
+                // console.log(weatherData)
                 $("#weatherDetails").show()
                 $("#cityName").text(weatherData.name)
                 $("#cityTemp").text(weatherData.main.temp)
                 $("#cityWind").text(weatherData.wind.speed)
                 $("#cityHumid").text(weatherData.main.humidity)
                 
-                console.log(weatherData.name)
-               
+                    addHistory(weatherData.name);
+                    showHistory();
+
+
             })
         }
 
@@ -36,9 +38,47 @@ var submitSearch = function(){
 
 }
 
+var addHistory = function(searchItem){
+   
+    var history = new Array()
+    var oldHistory = JSON.parse( localStorage.getItem("searchHistory"))
+    console.log(oldHistory)
+    if (oldHistory instanceof Array) history = oldHistory
+
+        if (history.includes(searchItem)){
+            
+        }
+
+         history.unshift(searchItem)
+         console.log(history)
+         localStorage.setItem("searchHistory",JSON.stringify(history))
+        // const p = document.createElement("p")
+        // p.innerHTML = (weatherData.name)
+        // $("#searchHistory").prepend(p);
+}
+
+
+var showHistory = function(){
+    $("#searchHistory").text("")
+    var history = JSON.parse(localStorage.getItem("searchHistory"))
+    if (history === null) history = new Array()
+    console.log(history)
+
+    let l = history.length
+    if ( l > 0){
+        for(i=0; i<l; i++){  
+          let p = document.createElement("p")
+            p.innerHTML = (history[i])
+            $("#searchHistory").append(p);
+
+        }
+    }        
+
+}
 
 
 $(document).ready(function(){
+    showHistory()
     let searchForm= document.getElementById("weatherSearch")
     searchForm.addEventListener("submit", (e) => {
       e.preventDefault();
@@ -46,4 +86,4 @@ $(document).ready(function(){
     })
 })
 
-alert("I havent finished but I am working on it ill push the updates as I go along I just need 2 more days super busy but the most important part works :)")
+// alert("I havent finished but I am working on it ill push the updates as I go along I just need 2 more days super busy but the most important part works :)")
