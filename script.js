d92636306d114ad968baf49621bdd14088f55327
Dave Allen <allendave007@gmail.com>
2023-04-03 23:21:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -19,20 +19,15 @@ var submitSearch = function(){
         if (cityLat !=0 && cityLon !=0) {
             var Url = "https://api.openweathermap.org/data/2.5/weather?lat="+cityLat+"&lon="+cityLon+"&appid="+apiKey+"&units=imperial"
                 $.get(Url,null,function(weatherData){
-                // console.log(weatherData)
+                console.log(weatherData)
                 $("#weatherDetails").show()
                 $("#cityName").text(weatherData.name)
                 $("#cityTemp").text(weatherData.main.temp)
                 $("#cityWind").text(weatherData.wind.speed)
                 $("#cityHumid").text(weatherData.main.humidity)
                 
-                console.log(weatherData.name)
-                localStorage.setItem('history', weatherData.name)
-               localStorage.getItem('history');
-            var history = weatherData.name
-                    const p = document.createElement("p")
-                    p.innerHTML = (history)
-                    document.getElementById("searchHistory").appendChild(p);
+                    addHistory(searchText);
+                    showHistory();
 
 
             })
@@ -43,14 +38,63 @@ var submitSearch = function(){
 
 }
 
+var addHistory = function(searchItem){
+   
+    var history = new Array()
+    var oldHistory = JSON.parse( localStorage.getItem("searchHistory"))
+    console.log(oldHistory)
+    if (oldHistory instanceof Array) history = oldHistory
 
+        if (history.includes(searchItem)){
+            for (i = 0; i < history.length; i++){
+                if (history[i]== searchItem){
+                    history.splice(i,1)
+                }
+
+                
+            }
+        }
+
+         history.unshift(searchItem)
+         console.log(history)
+         localStorage.setItem("searchHistory",JSON.stringify(history))
+        // const p = document.createElement("p")
+        // p.innerHTML = (weatherData.name)
+        // $("#searchHistory").prepend(p);
+}
+
+
+var showHistory = function(){
+    $("#searchHistory").text("")
+    var history = JSON.parse(localStorage.getItem("searchHistory"))
+    if (history === null) history = new Array()
+    console.log(history)
+
+    let l = history.length
+    if ( l > 0){
+        for(i=0; i<l; i++){  
+          let p = document.createElement("p")
+            p.innerHTML = (history[i])
+            $("#searchHistory").append(p);
+
+        }
+    }        
+
+}
+
+var clearHistory = function(){
+    localStorage.removeItem("searchHistory")
+    $("#searchHistory").text("")
+}
 
 $(document).ready(function(){
+    showHistory()
     let searchForm= document.getElementById("weatherSearch")
     searchForm.addEventListener("submit", (e) => {
       e.preventDefault();
         submitSearch()
     })
+    $("#ccHistory").on("click", clearHistory)
 })
 
 alert("I havent finished but I am working on it ill push the updates as I go along I just need 2 more days super busy but the most important part works :)")
